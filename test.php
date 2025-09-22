275d540297b34c535efa573785a36eed85cd3a52
Yehor Smoliakov <egorsmkv@gmail.com>
2025-09-22 20:21:46 +0300

    Enhance test file

--- test.php
+++ test.php
@@ -2,6 +2,13 @@
 
 require __DIR__ . '/packages/default/send-email/vendor/autoload.php';
 
+// Constants
+const OK = 1;
+
+const HTTP_OK = 200;
+
+const ERROR = -1;
+
 $data = json_encode([
     'name' => 'Yehor',
 ]);
@@ -24,12 +31,12 @@ $args = [
     'attachment_urls' => [
         [
             'filename' => 'test_1.pdf',
-            'url' => 'https://raw.githubusercontent.com/egorsmkv/do-functions-php-send-email/refs/heads/main/packages/default/send-email/test.pdf',
+            'url' => 'https://raw.githubusercontent.com/ElefantOne/do-functions-php-send-email/refs/heads/main/packages/default/send-email/test.pdf',
             'type' => 'application/pdf',
         ],
         [
             'filename' => 'test_2.pdf',
-            'url' => 'https://raw.githubusercontent.com/egorsmkv/do-functions-php-send-email/refs/heads/main/packages/default/send-email/test.pdf',
+            'url' => 'https://raw.githubusercontent.com/ElefantOne/do-functions-php-send-email/refs/heads/main/packages/default/send-email/test.pdf',
             'type' => 'application/pdf',
         ],
     ],
@@ -40,10 +47,43 @@ $client = new \GuzzleHttp\Client();
 $url = 'https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-d162e87e-b749-4ae7-ac9c-1a295057435a/default/send-email';
 
 $response = $client->post($url, [
+    // Config
+    'connect_timeout' => 20,
+    'read_timeout' => 20,
+    'timeout' => 30,
+    // The data
     'json' => $args,
 ]);
 
 $responseBody = $response->getBody()->getContents();
 $responseCode = $response->getStatusCode();
 
-echo $responseBody . PHP_EOL;
+if (HTTP_OK !== $responseCode) {
+    echo "Something went wrong!\n";
+
+    exit(1);
+}
+
+echo "HTTP call was successful!\n";
+
+/**
+ * @var array $parsedData
+ */
+$parsedData = json_decode($responseBody, true);
+
+/**
+ * @var array $response
+ */
+$response = $parsedData['response'];
+
+switch ($response['status']) {
+    case OK:
+        echo "Notification sent successfully!\n";
+        break;
+    case ERROR:
+        echo "Notification failed to send! Use parsedData variable to find out more.\n";
+        break;
+    default:
+        echo "Something went wrong! Use parsedData variable to find out more.\n";
+        break;
+}
